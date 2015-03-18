4a4a0b01d22e045296e57752d65fbc7a15bd7ffd
Tim Nagel <tim@nagel.com.au>
2015-03-18 13:20:38 +1100

    Helper method for View::create that handles serializerGroups without needing to create a SerializationContext

---
+++ View/View.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the Infinite Invocation project.
+ *
+ * (c) Infinite Networks Pty Ltd <http://www.infinite.net.au>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Infinite\ApiSupportBundle\View;
+
+use FOS\RestBundle\View\View as BaseView;
+use JMS\Serializer\SerializationContext;
+
+class View extends BaseView
+{
+    public static function create($data = null, $statusCode = null, array $headers = array(), array $groups = array())
+    {
+        $view = parent::create($data, $statusCode, $headers);
+
+        if ($groups) {
+            $context = new SerializationContext();
+            $context->setGroups($groups);
+            $view->setSerializationContext($context);
+        }
+
+        return $view;
+    }
+}
